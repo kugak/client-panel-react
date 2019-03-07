04264c337c53d86ce8bd0a1ec53e4f1a79245cd6
Kuga Kugaraja <kuga@Kugas-MacBook-Pro.local>
2019-03-07 11:51:06 -0500

    presist settings to localStorage

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -4,30 +4,24 @@ import {
   ALLOW_REGISTERATION
 } from "../actions/types";
 
-const initialState = {
-  disableBalanceOnAdd: true,
-  disableBalanceOnEdit: false,
-  allowRegistration: false
-};
-
-export default function(state = initialState, action) {
+export default function(state = {}, action) {
   switch (action.type) {
     case DISABLE_BALANCE_ON_ADD:
       return {
         ...state,
-        disableBalanceOnAdd: !state.disableBalanceOnAdd
+        disableBalanceOnAdd: action.payload
       };
 
     case DISABLE_BALANCE_ON_EDIT:
       return {
         ...state,
-        disableBalanceOnEdit: !state.disableBalanceOnEdit
+        disableBalanceOnEdit: action.payload
       };
 
     case ALLOW_REGISTERATION:
       return {
         ...state,
-        allowRegistration: !state.allowRegistration
+        allowRegistration: action.payload
       };
 
     default:
